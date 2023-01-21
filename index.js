3c7ea5c577737297f31db2cd1a4d0fdf3c652aec
doonee <doonee@outlook.com>
2023-01-21 17:10:57 +0900

    added dotenv.config option

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-//require('dotenv').config()
+require('dotenv').config()
 // const port = process.env.REACT_APP_PORT
 
 // // post로 보내는 값 받는 옵션
